2759ccfc165ddb26c577f8789297d5fed0c31b7c
Torao-Law <dandisyahrullah.12@gmail.com>
2023-09-13 21:35:56 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -12,14 +12,14 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path='/' element={<Home />} />
-        <Route exact path='/currency' element={<CurrencyConverter />} />
-        <Route exact path='/count' element={<Countdown />} />
-        <Route exact path='/mobile-legend' element={<MobileLegends />} />
-        <Route exact path='/tic-tac' element={<Tictac />} />
-        <Route exact path='/matching-card' element={<Matching />} />
-        <Route exact path='/salary' element={<Salary />} />
-        <Route exact path='/word' element={<WordScramble />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/currency' element={<CurrencyConverter />} />
+        <Route path='/count' element={<Countdown />} />
+        <Route path='/mobile-legend' element={<MobileLegends />} />
+        <Route path='/tic-tac' element={<Tictac />} />
+        <Route path='/matching-card' element={<Matching />} />
+        <Route path='/salary' element={<Salary />} />
+        <Route path='/word' element={<WordScramble />} />
       </Routes>
     </Router>
   )
